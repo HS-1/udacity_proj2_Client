10f46c3b38145bf963dbc974e9ccb62362c0d8b4
HS-1 <66611834+HS-1@users.noreply.github.com>
2021-06-16 11:20:32 +0800

    Update __init__.py

--- NeighborlyAPI/createAdvertisement/__init__.py
+++ NeighborlyAPI/createAdvertisement/__init__.py
@@ -7,7 +7,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if request:
         try:
-            url = "localhost"  # TODO: Update with appropriate MongoDB connection information
+            url = "AccountEndpoint=https://hsneighborlycosmosacc.documents.azure.com:443/;AccountKey=h56x0NaruDZCCp1JVYNGHteH9rJd9XXHTdEEZtWd0PSaI4IAeRVHFKNMjnScEzlWGNf7ARluxvPa7PUkutVfOA==;"  # TODO: Update with appropriate MongoDB connection information
             client = pymongo.MongoClient(url)
             database = client['azure']
             collection = database['advertisements']
