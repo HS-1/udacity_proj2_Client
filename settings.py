4fc65eef996de1ac14503749a944c6018c469ca2
HS-1 <66611834+HS-1@users.noreply.github.com>
2021-06-22 10:57:19 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 #--------- Flask settings
-SERVER_HOST = "https://hsproj2webappclient.azurewebsites.net/" # Update this for the appropriate front-end website when up
+SERVER_HOST = 'https://hsproj2webappclient.azurewebsites.net/' # Update this for the appropriate front-end website when up
 SERVER_PORT = 5000
 FLASK_DEBUG = True # Do not use debug mode in prod
 
